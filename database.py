8b37366f82f60b72a7e99765b9357538fb5e98e1
LEVY4TECH <levy4tech@gmail.com>
2025-05-06 14:45:02 -0700

    5/6

--- database.py
+++ database.py
@@ -24,10 +24,34 @@ def insert_sales(values):
     cur.execute(insert,values)
     conn.commit()
 
-# def profit_per_product():
-#     cur.execute("select products.productname, sum((products.selling_price-products.buying_price)*sales.quantity) as profit from products join sales on products.productid=sales.productid group by (products.productname);")
-#     profit_per_product=cur.fetchall()
-#     return profit_per_product
+def profit_per_product():
+    cur.execute("select products.productname, sum((products.selling_price-products.buying_price)*sales.quantity) as profit from products join sales on products.productid=sales.productid group by (products.productname);")
+    profit_per_product=cur.fetchall()
+    return profit_per_product
 
-# profit_product=profit_per_product()
+profit_product=profit_per_product()
 # print(profit_product)
+
+def profit_per_day():
+    cur.execute("select date(sales.created_at), sum((products.selling_price-products.buying_price)*sales.quantity) as profit from products join sales on products.productid=sales.productid group by (sales.created_at);")
+    profit_per_day=cur.fetchall()
+    return profit_per_day
+
+profit_day=profit_per_day()
+# print(profit_day)
+
+def sales_per_product():
+    cur.execute("select products.productname, sum(products.selling_price*sales.quantity) as total_sales from products join sales on products.productid=sales.productid group by (products.productname);")
+    sales_per_product=cur.fetchall()
+    return sales_per_product
+
+sales_product=sales_per_product()
+# print(sales_product)
+
+def sales_per_day():
+    cur.execute("select date(sales.created_at), sum(products.selling_price*sales.quantity) as total_sales from products join sales on products.productid=sales.productid group by (sales.created_at);")
+    sales_per_day=cur.fetchall()
+    return sales_per_day
+
+sales_day=sales_per_day()
+# print(sales_day)
